62df6f5c4740a90bd805ec08daf357004d29cdbd
dioscarr <Dioscard@hotmail.com>
2015-08-28 10:04:55 -0400

    --Created array to hold building in memory and display them in the UI--Added a form to insert new buildings to the table--there is still some errors that need to be solves--

--- BMApp/BMApp/Scripts/CustomScripts/Management.js
+++ BMApp/BMApp/Scripts/CustomScripts/Management.js
@@ -6,25 +6,25 @@
         //    { Name: ko.observable('John Doe'), Tel: '347566998', Email: 'John@gmail.com' },
         //    { Name: ko.observable('Carry smith'), Tel: '347566998', Email: 'John@gmail.com' },
         //    { Name: ko.observable('Sam George'), Tel: '347566998', Email: 'John@gmail.com' },
-        //    //{ Name: ko.observable('John Doe'), Tel: '347566998', Email: 'John@gmail.com' },
-        //    //{ Name: ko.observable('Carry smith'), Tel: '347566998', Email: 'John@gmail.com' },
-        //    //{ Name: ko.observable('Sam George'), Tel: '347566998', Email: 'John@gmail.com' },
-        //    //{ Name: ko.observable('John Doe'), Tel: '347566998', Email: 'John@gmail.com' },
-        //    //{ Name: ko.observable('Carry smith'), Tel: '347566998', Email: 'John@gmail.com' },
-        //    //{ Name: ko.observable('Sam George'), Tel: '347566998', Email: 'John@gmail.com' },
-        //    //{ Name: ko.observable('John Doe'), Tel: '347566998', Email: 'John@gmail.com' },
-        //    //{ Name: ko.observable('Carry smith'), Tel: '347566998', Email: 'John@gmail.com' },
-        //    //{ Name: ko.observable('Sam George'), Tel: '347566998', Email: 'John@gmail.com' },
-        //    //{ Name: ko.observable('John Doe'), Tel: '347566998', Email: 'John@gmail.com' },
-        //    //{ Name: ko.observable('Carry smith'), Tel: '347566998', Email: 'John@gmail.com' },
-        //    //{ Name: ko.observable('Sam George'), Tel: '347566998', Email: 'John@gmail.com' },
-        //    //{ Name: ko.observable('John Doe'), Tel: '347566998', Email: 'John@gmail.com' },
-        //    //{ Name: ko.observable('Carry smith'), Tel: '347566998', Email: 'John@gmail.com' },
-        //    //{ Name: ko.observable('Sam George'), Tel: '347566998', Email: 'John@gmail.com' },
-        //    //{ Name: ko.observable('John Doe'), Tel: '347566998', Email: 'John@gmail.com' },
-        //    //{ Name: ko.observable('Carry smith'), Tel: '347566998', Email: 'John@gmail.com' },
-        //    //{ Name: ko.observable('Sam George'), Tel: '347566998', Email: 'John@gmail.com' },
-        //    //{ Name: ko.observable('John Doe'), Tel: '347566998', Email: 'John@gmail.com' },
+        //    { Name: ko.observable('John Doe'), Tel: '347566998', Email: 'John@gmail.com' },
+        //    { Name: ko.observable('Carry smith'), Tel: '347566998', Email: 'John@gmail.com' },
+        //    { Name: ko.observable('Sam George'), Tel: '347566998', Email: 'John@gmail.com' },
+        //    { Name: ko.observable('John Doe'), Tel: '347566998', Email: 'John@gmail.com' },
+        //    { Name: ko.observable('Carry smith'), Tel: '347566998', Email: 'John@gmail.com' },
+        //    { Name: ko.observable('Sam George'), Tel: '347566998', Email: 'John@gmail.com' },
+        //    { Name: ko.observable('John Doe'), Tel: '347566998', Email: 'John@gmail.com' },
+        //    { Name: ko.observable('Carry smith'), Tel: '347566998', Email: 'John@gmail.com' },
+        //    { Name: ko.observable('Sam George'), Tel: '347566998', Email: 'John@gmail.com' },
+        //    { Name: ko.observable('John Doe'), Tel: '347566998', Email: 'John@gmail.com' },
+        //    { Name: ko.observable('Carry smith'), Tel: '347566998', Email: 'John@gmail.com' },
+        //    { Name: ko.observable('Sam George'), Tel: '347566998', Email: 'John@gmail.com' },
+        //    { Name: ko.observable('John Doe'), Tel: '347566998', Email: 'John@gmail.com' },
+        //    { Name: ko.observable('Carry smith'), Tel: '347566998', Email: 'John@gmail.com' },
+        //    { Name: ko.observable('Sam George'), Tel: '347566998', Email: 'John@gmail.com' },
+        //    { Name: ko.observable('John Doe'), Tel: '347566998', Email: 'John@gmail.com' },
+        //    { Name: ko.observable('Carry smith'), Tel: '347566998', Email: 'John@gmail.com' },
+        //    { Name: ko.observable('Sam George'), Tel: '347566998', Email: 'John@gmail.com' },
+        //    { Name: ko.observable('John Doe'), Tel: '347566998', Email: 'John@gmail.com' },
         //    { Name: ko.observable('Carry smith'), Tel: '347566998', Email: 'John@gmail.com' },
         //    { Name: ko.observable('Sam George'), Tel: '347566998', Email: 'John@gmail.com' }
         //    );
@@ -34,7 +34,7 @@
 
         var url = "http://www.filltext.com/?callback=?";
         $.getJSON(url, {
-            'rows': 50,
+            'rows': 100,
             'id':'{number}',
             'Name': '{firstName}',
             'Tel': '{phone}',
@@ -50,9 +50,9 @@
         self.employeeResult = ko.observableArray();
         self.query = ko.observable('');
         //Remove employee
-        self.removeEmp = function (data) {
-            alert(JSON.stringify(data));
-            alert(data);
+        self.removeEmp = function (data, event) {
+           
+            alert();
         }
 
         //compares strings from name with the text just entered in the textbox
@@ -90,19 +90,94 @@
                 Tel:  self.AddTel('');
                 Email: self.AddEmail('');
             }
-      //load 
-        self.LoadPermissions = function () {
-            $.ajax({
-                type:"GET",
-                url: "/Management/LoadPermissions/",
-                datatype: "json",
-                success: function (returnPermissions) {
-                    var Permissions = JSON.stringify(returnPermissions.Data);  
-                }               
-            });
-        }
-        self.LoadPermissions();
+      ////load 
+      //  self.LoadPermissions = function () {
+      //      $.ajax({
+      //          type:"GET",
+      //          url: "/Management/LoadPermissions/",
+      //          datatype: "json",
+      //          success: function (returnPermissions) {
+      //              var Permissions = JSON.stringify(returnPermissions.Data);  
+      //          }               
+      //      });
+      //  }
+        //  self.LoadPermissions();
+
+        //Setting Company
+            self.CompanyName = ko.observable();
+            self.CompanyAddress = ko.observable();
+
+            self.AddCompany = function () {
+
+                compantlist.push({
+                    'id': ko.observable(self.CompanyName()),
+                    'Name': ko.observable('Brandon'),
+                    'Address': ko.observable(self.CompanyAddress())
+                });
+               //self.Companies.removeAll();
+               //self.Companies(compantlist);
+
+
+             
+                    $('#managementtable').find('thead tr').each(function () {
+                        $(this).find('td:last').after('<td>' + self.CompanyAddress() + '</td>');
+                    });
+
+                    $('#managementtable').find('tbody tr').each(function () {
+                        $(this).find('td:last').after('<td><div class="checkbox" > <label class="checkPermission"> <input type="checkbox" > Add Permission </label></div></td>');
+                    });
+               
+
+
+
+
+            }
+
+
+       
+        //Adding company column on the table
+            var compantlist = [
+                { 'id': ko.observable('1'), 'Name': ko.observable('Brandon'), 'Address': ko.observable('1411 Broadway New York, NY 10018') },
+                { 'id': ko.observable('2'), 'Name': ko.observable('LTD & Corp'), 'Address': ko.observable('1412 Broadway New York, NY 10018') },
+                { 'id': ko.observable('3'), 'Name': ko.observable('Realty Properties'), 'Address': ko.observable('1413 Broadway New York, NY 10018') },
+                { 'id': ko.observable('4'), 'Name': ko.observable('Simon LTD'), 'Address': ko.observable('1440 Broadway New York, NY 10018') },
+                 { 'id': ko.observable('2'), 'Name': ko.observable('LTD & Corp'), 'Address': ko.observable('1412 Broadway New York, NY 10018') },
+                { 'id': ko.observable('3'), 'Name': ko.observable('Realty Properties'), 'Address': ko.observable('1413 Broadway New York, NY 10018') },
+                { 'id': ko.observable('4'), 'Name': ko.observable('Simon LTD'), 'Address': ko.observable('1440 Broadway New York, NY 10018') },
+                { 'id': ko.observable('5'), 'Name': ko.observable('Pointer Realty'), 'Address': ko.observable('1375 Broadway New York, NY 10018') },
+            ];
+
+            self.Companies = ko.observableArray(compantlist);
+
+            loaddata();
+            function loaddata()
+            {
+                for (var i = 0; i < self.Companies().length; i++) {
+                    $('#managementtable').find('thead tr').each(function () {
+
+                        $(this).find('td:last').after('<td>' + compantlist[i].Address() + '</td>');
+                    });
+
+                    $('#managementtable').find('tbody tr').each(function () {
+
+                        $(this).find('td:last').after('<td><div class="checkbox" > <label class="checkPermission"> <input type="checkbox" > Add Permission </label></div></td>');
+                    });
+                }
+            }
+           
+           
+
+
+     
+
     }
     ko.applyBindings(viewModel);
+
+   
+
+
+
+
+   
    
 });
